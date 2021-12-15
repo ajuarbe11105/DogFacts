077e4047274948cb3d715a71bfa8b8b00190e7f9
alberto Juarbe <albertojuarbe2@gmail.com>
2021-12-15 16:40:36 -0500

    Initial Commit

---
+++ DogFacts/DogFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFactsApp.swift
+//  DogFacts
+//
+//  Created by Alberto Juarbe on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DogFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
